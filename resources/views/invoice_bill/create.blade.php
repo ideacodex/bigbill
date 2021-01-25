3fa8d6ab1461166527a028d1c445ccb288913af6
Maria Sarazua <sarazuamaria@gmail.com>
2021-01-25 09:47:16 -0600

    Arreglo en tabla de factura

--- resources/views/invoice_bill/create.blade.php
+++ resources/views/invoice_bill/create.blade.php
@@ -113,6 +113,7 @@
                         <table id="TablaPro" class="table">
                             <thead>
                                 <tr>
+                                    <th>Código de producto</th>
                                     <th>Producto</th>
                                     <th>Cantidad</th>
                                     <th>Precio</th>
