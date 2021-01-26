8dfe06dce5844c916dca367a7a55ce389bd600a3
GEOVARU <gramirez@pctecbus.co>
2021-01-26 08:31:16 -0600

    actualizacion de tarjeta de presentacion de usuario

---
+++ resources/views/usuario/edit.blade.php
@@ -0,0 +1,176 @@
+@extends('layouts.Administrador')
+@section('content')
+    <!--Validación de errores-->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <!--Validación de errores-->
+
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Actualizar Informacion</strong>
+                        </div>
+                        <div class="card-body">
+                            <div>
+                                <form action="{{ url('home/' . $user->id) }}" method="POST" enctype="multipart/form-data"
+                                    onsubmit="return checkSubmit();">
+                                    @csrf @method('PATCH')
+                                    <!--Role Id-->
+                                    <!--
+                                        <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                    <i title="Nombre" class="fas fa-people-carry"></i>
+                                                </span>
+                                            </div>
+                                            <input readonly="readonly "id="role_id" name="role_id" type="text"
+                                                class="text-dark form-control @error('role_id') is-invalid @enderror"
+                                                value="{{ $user->role_id }}" placeholder="Id" required
+                                                autocomplete="role_id" autofocus>
+                                            @error('role_id')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                            @enderror
+                                        </div>
+                                        -->
+
+                                    <!--Nombre-->
+                                    <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                <i class="fas fa-user"></i>
+                                            </span>
+                                        </div>
+                                        <input id="name" name="name" type="text"
+                                            class="text-dark form-control @error('name') is-invalid @enderror"
+                                            value="{{ $user->name }}" placeholder="Nombre" required autocomplete="name"
+                                            autofocus>
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <!-- lastname -->
+                                    <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                <i class="far fa-user"></i>
+                                            </span>
+                                        </div>
+                                        <input id="lastname" name="lastname" type="text"
+                                            class="text-dark form-control @error('lastname') is-invalid @enderror"
+                                            value="{{ $user->lastname }}" placeholder="Apellidos" required
+                                            autocomplete="lastname" autofocus>
+                                        @error('lastname')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <!-- phone -->
+                                    <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                <i class="fas fa-phone"></i>
+                                            </span>
+                                        </div>
+                                        <input id="phone" name="phone" type="text"
+                                            class="text-dark form-control @error('phone') is-invalid @enderror"
+                                            value="{{ $user->phone }}" placeholder="Telefono" required autocomplete="phone"
+                                            autofocus>
+                                        @error('phone')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <!--  nit -->
+                                    <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                <i class="fas fa-id-badge"></i>
+                                            </span>
+                                        </div>
+                                        <input id="nit" name="nit" type="text"
+                                            class="text-dark form-control @error('nit') is-invalid @enderror"
+                                            style="color:white" value="{{ $user->nit }}" placeholder="nit" required
+                                            autocomplete="nit" autofocus>
+                                        @error('nit')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <!--  address -->
+                                    <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                <i class="fas fa-map-marker-alt"></i>
+                                            </span>
+                                        </div>
+                                        <input id="address" name="address" type="text"
+                                            class="text-dark form-control @error('address') is-invalid @enderror"
+                                            value="{{ $user->address }}" placeholder="guatemala, guatemla" required
+                                            autocomplete="address" autofocus>
+                                        @error('address')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <!-- email  -->
+                                    <div class="col-12 col-md-6 input-group input-group-lg mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text transparent" id="inputGroup-sizing-sm">
+                                                <i class="fas fa-envelope"></i>
+                                            </span>
+                                        </div>
+                                        <input id="email" name="email" type="text"
+                                            class="text-dark form-control @error('email') is-invalid @enderror"
+                                            value="{{ $user->email }}" placeholder="email@extension.com" required
+                                            autocomplete="email" autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <!--Button-->
+                                    <div class="container mt-4">
+                                        <div class="col-12">
+                                            <div class="col text-center">
+                                                <button type="submit" style="border-radius: 10px"
+                                                    class="btn btn-lg btn-primary mt-3">
+                                                    <i class="far fa-save"></i>
+                                                    {{ __('Actualizar Informacion') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+
+@endsection
--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -1,19 +1,70 @@
-
 @extends('layouts.Administrador')
 @section('content')
 
-
-    <!--mensajes -->
-    <div class="content mt-3">
-        <div class="col-sm-12">
-            <div class="alert  alert-success alert-dismissible fade show" role="alert">
-                <span class="badge badge-pill badge-success">Atencion</span> Usted a ingresado correctamente
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-    </div> <!-- .content -->
-    <!--mensajes -->
+    @endif
+    <!--Validación de errores-->
+    <!--Mensaje flash-->
+    @if (session('MENSAJEEXITOSO'))
+        <div class="alert  alert-success alert-dismissible fade show" role="alert">
+            <div class="alert alert-success">
+                {{ session('MENSAJEEXITOSO') }}
+            </div>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+
+
+
+
+
+    <div class="col-md-9" style="margin-left:10%; " >
+        <aside class="profile-nav alt " >
+            <section class="card">
+                <div class="card-header user-header alt bg-dark">
+                    <div class="media">
+                       
+                            <img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt=""
+                                src="images/admin.jpg">
+                        
+                        <div class="media-body">
+                            <h2 class="text-light display-6"> {{ Auth::user()->name }} {{ Auth::user()->lastname }}</h2>
+                            <p>{{ Auth::user()->email }}</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <a href="#"> <i class="fas fa-phone-alt"></i>{{ Auth::user()->phone }}</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="#"> <i class="fa fa-tasks"></i> {{ Auth::user()->address }}</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="#"> <i class="fa fa-bell-o"></i> {{ Auth::user()->nit }}</a>
+                    </li>
+                
+                    <a class="btn btn-sm btn-primary" href="{{ url('home/' . Auth::user()->id . '/edit') }}" title="Editar">
+                        <span><i class="fas fa-edit"></i></span>
+                    </a>
+
+                </ul>
+
+            </section>
+        </aside>
+    </div>
+
 
 @endsection
