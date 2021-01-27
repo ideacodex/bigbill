24edca27f622464dc69abcc415e71dc6be042997
GEOVARU <gramirez@pctecbus.co>
2021-01-27 12:29:10 -0600

    creacion de documento de factura

---
+++ database/migrations/2021_01_14_192355_detail_bill.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DetailBill extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('detail_bills', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('invoice_id');
+            $table->foreign('invoice_id')
+            ->references('id')->on('invoice_bills');
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')
+            ->references('id')->on('products');
+            $table->decimal('quantity');
+            $table->decimal('unit_price');
+            $table->decimal('subtotal');
+            $table->timestamps();           
+        });
+
+        
+    }   
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_bill');
+    }
+}
---
+++ database/seeds/Roles.php
@@ -0,0 +1,20 @@
+<?php
+
+use DB;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Contracts\Role;
+use Spatie\Permission\Models\Permission;
+
+class Roles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+       
+    }
+}
