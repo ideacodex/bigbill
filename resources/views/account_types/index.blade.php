27c440c8cd6a15e67c814f4a990b8653a39eddf6
Maria Sarazua <sarazuamaria@gmail.com>
2021-03-08 16:56:30 -0600

    Cambio de diseño en vista de tablas

--- resources/views/account_types/index.blade.php
+++ resources/views/account_types/index.blade.php
@@ -1,122 +1,127 @@
 @extends('layouts.'. auth()->user()->getRoleNames()[0])
 @section('content')
 
-<!--Validación de errores-->
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-<!--Validación de errores-->
-<!--Mensaje flash-->
-@if (session('datosEliminados'))
-<div class="sufee-alert alert with-close alert-danger alert-dismissible fade show">
-    <span class="badge badge-pill badge-danger">Atención</span>
-    {{ session('datosEliminados') }}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-@endif
-<!--Mensaje flash-->
+    <!--Validación de errores-->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <!--Validación de errores-->
+    <!--Mensaje flash-->
+    @if (session('datosEliminados'))
+        <div class="sufee-alert alert with-close alert-danger alert-dismissible fade show">
+            <span class="badge badge-pill badge-danger">Atención</span>
+            {{ session('datosEliminados') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    <!--Mensaje flash-->
 
-<div class="content mt-3">
-    <div class="animated fadeIn">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <strong class="card-title">Tipos de cuentas</strong>
-                    </div>
+    <div class="card-body d-flex justify-content-between align-items-center">
+        <button type="button" style="border-radius: 95px;" class="btn btn-success mb-1 ml-2 mt-2" data-toggle="modal"
+            data-target="#exampleModal" data-whatever="@mdo">
+            + AGREGAR TIPO DE CUENTA
+        </button>
+    </div>
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header" style="background-color: black; color: white; border-radius: 15px">
+                            <strong class="card-title">Tipos de cuentas</strong>
+                        </div>
+                        <div class="card-body">
+                            <div class="row table-responsive">
+                                <div class="col-12">
+                                    <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                                        <thead style="border-radius: 15px; background-color: black; color:white">
+                                            <tr>
+                                                <th>No. </th>
+                                                <th>Tipo de cuenta</th>
+                                                <th>Acciones</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody style="background-color: rgba(224, 220, 220, 0.993);">
+                                                      @foreach ($account_types as $item)
+                                            <tr>
+                                                <th style="border-left: #325ff5 7px solid;"
+                                                    title="{{ $loop->index + 1 }}">{{ $loop->index + 1 }}</th>
+                                                <td title="{{ $item->status }}">{{ $item->status }}</td>
+                                                <td>
+                                                    <div class="btn-group" role="group" aria-label="Basic example">
+                                                        <a title="Actualizar Tipo de Cuenta"
+                                                            class="btn btn-sm btn-primary rounded-circle"
+                                                            href="{{ url('TipodeCuenta/' . $item->id . '/edit') }}"
+                                                            title="Editar">
+                                                            <span><i class="fas fa-edit"></i></span>
+                                                        </a>
 
-                    <div class="card-body d-flex justify-content-between align-items-center">
-                        <button type="button" style="border-radius: 95px;" class="btn btn-success mb-1 ml-2 mt-2" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
-                            Agregar tipo de cuenta
-                            <i class="fas fa-plus-square"></i>
-                        </button>
-                    </div>
-                    <div class="card-body">
-                        <div class="row table-responsive">
-                            <div class="col-12">
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
 
-
-                                <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Tipo de cuenta</th>
-                                            <th>Acciones</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($account_types as $item)
-                                        <tr>
-                                            <th title="{{ $loop->index + 1 }}"  >{{ $loop->index + 1 }}</th>
-                                            <td title="{{ $item->status }}" >{{ $item->status }}</td>
-                                            <td>
-                                                <div class="btn-group" role="group" aria-label="Basic example">
-
-                                                    <a   title="Actualizar Tipo de Cuenta" class="btn btn-sm btn-primary" href="{{ url('TipodeCuenta/' . $item->id . '/edit') }}" title="Editar">
-                                                        <span><i class="fas fa-edit"></i></span>
-                                                    </a>
-
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
+                </div>
+            </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Nuevo tipo de cuenta</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="{{ route('TipodeCuenta.store') }}" onsubmit="return checkSubmit();">
+                        @csrf
+                        <div class="form-group">
+                            <input id="status" type="text"
+                                class="text-dark form-control @error('status') is-invalid @enderror" name="status"
+                                value="{{ old('status') }}" placeholder="Estado financiero" required autocomplete="status"
+                                autofocus>
+                        </div>
+
+
+                        <div class="container mt-4">
+                            <div class="col-12">
+                                <div class="col text-center">
+                                    <button type="submit" style="border-radius: 10px" class="btn btn-lg btn-primary mt-3">
+                                        <i class="far fa-save"></i>
+                                        {{ __('Guardar') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button style="border-radius: 20%" type="button" class="btn btn-danger" data-dismiss="modal"><i
+                            class="fas fa-times-circle"></i> Cerrar
+                    </button>
                 </div>
             </div>
         </div>
-    </div><!-- .animated -->
-</div><!-- .content -->
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Nuevo tipo de cuenta</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form method="POST" action="{{ route('TipodeCuenta.store') }}" onsubmit="return checkSubmit();">
-                    @csrf
-                    <div class="form-group">
-                        <input id="status" type="text" class="text-dark form-control @error('status') is-invalid @enderror" name="status" value="{{ old('status') }}" placeholder="Estado financiero" required autocomplete="status" autofocus>
-                    </div>
-
-
-                    <div class="container mt-4">
-                        <div class="col-12">
-                            <div class="col text-center">
-                                <button type="submit" style="border-radius: 10px" class="btn btn-lg btn-primary mt-3">
-                                    <i class="far fa-save"></i>
-                                    {{ __('Guardar') }}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button style="border-radius: 20%" type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times-circle"></i> Cerrar
-                </button>
-            </div>
-        </div>
     </div>
-</div>
 
 @endsection
--- resources/views/layouts/Administrador.blade.php
+++ resources/views/layouts/Administrador.blade.php
@@ -60,7 +60,6 @@
         max-height: 230px;
     }
 
-
     /* Animation */
     .parallax>use {
         animation: move-forever 25s cubic-bezier(.55, .5, .45, .5) infinite;
@@ -112,9 +111,25 @@
         }
     }
 
+    .FondoParteDeOndasDecabeza {
+        width: 100%;
+        position: absolute;
+        top: 0%;
+    }
+
     .FondoParteDeOndas {
-        color: white;
-        background: linear-gradient(70deg, rgb(79, 53, 185) 0%, rgb(0, 182, 206) 100%);
+        height: 50px;
+    }
+
+    .FondoParteDeOndasNormal {
+        width: 50%;
+        bottom: 0%;
+        float: right;
+    }
+
+    img.derecha {
+        float: right;
+        width: 10%;
     }
 
 </style>
@@ -427,35 +442,35 @@
                         d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                 </defs>
                 <g class="parallax">
-                    <use xlink:href="#gentle-wave" x="40" y="0" fill="rgba(86,174,249,0.7" />
-                    <use xlink:href="#gentle-wave" x="35" y="3" fill="#c9dded" />
-                    <use xlink:href="#gentle-wave" x="25" y="6" fill="#bcddf9" />
-                    <use xlink:href="#gentle-wave" x="10" y="12" fill="#a9dcfb" />
-                    <use xlink:href="#gentle-wave" x="48" y="9" fill="#a9dcfb" />
+                    <use xlink:href="#gentle-wave" x="40" y="0" fill="rgba(133,184,255,0.5)" />
+                    <use xlink:href="#gentle-wave" x="25" y="-6" fill="rgba(124,198,255,0.3)" />
+                    <use xlink:href="#gentle-wave" x="25" y="-6" fill="rgba(124,198,255,0.3)" />
+                    <use xlink:href="#gentle-wave" x="35" y="-3" fill="rgba(140,210,253,0.7" />
+                    <use xlink:href="#gentle-wave" x="48" y="-9" fill="rgba(255,255,255,0.3)" />
                 </g>
             </svg>
         </div>
         {{-- ------------------------- --}}
 
         @yield('content')
+        
+        <img class="derecha" src="{{ asset('images/ideacode.png') }}">
 
-
-        <div id="FondoParteDeOndas" class="">
-            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        {{-- <div id="FondoParteDeOndasNormal" class="FondoParteDeOndasNormal">
+            <svg class="wavesNormal" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                 viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
                 <defs>
-                    <path id="gentle-wave"
-                        d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                 </defs>
                 <g class="parallax">
-                    <use xlink:href="#gentle-wave" x="40" y="0" fill="rgba(86,174,249,0.7" />
-                    <use xlink:href="#gentle-wave" x="35" y="3" fill="#c9dded" />
-                    <use xlink:href="#gentle-wave" x="25" y="6" fill="#bcddf9" />
-                    <use xlink:href="#gentle-wave" x="10" y="12" fill="#a9dcfb" />
-                    <use xlink:href="#gentle-wave" x="48" y="9" fill="#a9dcfb" />
+                    <use xlink:href="#gentle-wave" x="40" y="0" fill="rgba(133,184,255,0.5)" />
+                    <use xlink:href="#gentle-wave" x="25" y="6" fill="rgba(124,198,255,0.3)" />
+                    <use xlink:href="#gentle-wave" x="25" y="6" fill="rgba(124,198,255,0.3)" />
+                    <use xlink:href="#gentle-wave" x="35" y="3" fill="rgba(140,210,253,0.7" />
+                    <use xlink:href="#gentle-wave" x="48" y="9" fill="rgba(255,255,255,0.3)" />
                 </g>
             </svg>
-        </div>
+        </div> --}}
 
     </div><!-- /#right-panel -->
     <!-- Right Panel -->
