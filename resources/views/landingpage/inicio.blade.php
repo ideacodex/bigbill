15889ce0887cbf913381372712ff8a9c781ece16
geovanni <gramirez@pctecbus.co>
2021-03-15 10:39:10 -0600

    Actualizacion de landingpage

--- resources/views/landingpage/inicio.blade.php
+++ resources/views/landingpage/inicio.blade.php
@@ -162,9 +162,9 @@
                     {{-- SEGURIDADEN LA NUBE --}}
                     <div class="card rounded" style="background-color: #ffffff;">
                         <div class="bg-primary media pt-3 pb-5">
-                            <img src="img/Computadora-Telefono.svg" class="m-3" height="60px">
+                            <img src="img/ICONO_SEGURIDAD_EN_LA_NUBE.svg" class="m-3" height="60px">
                             <div class="media-body">
-                                <h5 class="mt-0 mr-2 ml-2 text-light ">
+                                <h5 class="mt-0 mr-2 ml-2 text-light Rubik-Medium ">
                                     SEGURIDADEN LA NUBE
                                 </h5>
                             </div>
@@ -178,22 +178,21 @@
                         </div>
                     </div>
 
-                    {{-- MANEJO DE MÚLTIPLESEMPRESAS --}}
+                    {{-- MANEJO DE MÚLTIPLES EMPRESAS --}}
 
                     <div class="card rounded" style="background-color: #ffffff;">
                         <div class="bg-primary media pt-3 pb-5">
-                            <img src="img/Computadora-Telefono.svg" class="m-3" height="60px">
+                            <img src="img/ICONO_MANEJO_MULTIPLE_EMPRESAS.svg" class="m-3" height="60px">
                             <div class="media-body">
-                                <h5 class="mt-0 mr-2 ml-2 text-light">
-                                    SEGURIDADEN LA NUBE
+                                <h5 class="mt-0 mr-2 ml-2 text-light Rubik-Medium">
+                                    MANEJO DE MÚLTIPLES EMPRESAS
                                 </h5>
                             </div>
                         </div>
                         <div style="background-color: #ffffff; border-radius: 100% 100% 0 0/ 20% 20% 0 0; margin-top: -3em;"
                             class="card-text">
-                            <p class="pt-5 ml-4 mr-4">
-                                Solo tú tienes acceso a tus documentos por medio de tu contraseña. Toda tu información se
-                                encripta de forma segura BIG BILL te proporciona la más alta privacidad y seguridad.
+                            <p class="pt-5 ml-4 mr-4 Rubik-Medium">
+                                Puedes tener trabajando más de una empresa desde la misma cuenta sin ningún problema.
                             </p>
                         </div>
                     </div>
@@ -202,31 +201,29 @@
                     {{-- EXPERIENCIA INTEGRAL --}}
                     <div class="card rounded" style="background-color: #ffffff;">
                         <div class="bg-primary media pt-3 pb-5">
-                            <img src="img/Computadora-Telefono.svg" class="m-3" height="60px">
+                            <img src="img/ICONO_EXPERIENCIA_INTEGRAL.svg" class="m-3" height="60px">
                             <div class="media-body">
-                                <h5 class="mt-0 mr-2 ml-2 text-light">
-                                    SEGURIDADEN LA NUBE
+                                <h5 class="mt-0 mr-2 ml-2 text-light Rubik-Medium">
+                                    EXPERIENCIA INTEGRAL
                                 </h5>
                             </div>
                         </div>
                         <div style="background-color: #ffffff; border-radius: 100% 100% 0 0/ 20% 20% 0 0; margin-top: -3em;"
                             class="card-text">
-                            <p class="pt-5 ml-4 mr-4">
-                                Solo tú tienes acceso a tus documentos por medio de tu contraseña. Toda tu información se
-                                encripta de forma segura BIG BILL te proporciona la más alta privacidad y seguridad.
+                            <p class="pt-5 ml-4 mr-4 Rubik-Medium">
+                                Que la experiencia de navegación de los usuarios sea amena, clara y ordenada, para que todos
+                                puedan poner en marcha casi al instanteusus procesos administrativos.
                             </p>
                         </div>
                     </div>
-
-
-
-
+                   
                 </div>
+                <br><br><br><br>
             </div>
         </div>
     </div>
 
-    <br><br><br><br>
+
 
 
 
