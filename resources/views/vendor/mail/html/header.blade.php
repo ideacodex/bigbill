4f52d333f6cabbd95288fbf5c0f394761c0ea3d7
Maria Sarazua <sarazuamaria@gmail.com>
2021-03-24 11:42:13 -0600

    Este sí, te lo prometo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header" align="center" style="background-color: black" width="570" cellpadding="0" cellspacing="0">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img style="width: 50%" class="user-avatar" src="{{ asset('images/LBB.png') }}" alt="Big Bill">
+                <img style="width: 35%" class="user-avatar" src="https://bigbill.co/img/logo_tageta.png" alt="Big Bill">
             @else
                 {{ $slot }}
             @endif
