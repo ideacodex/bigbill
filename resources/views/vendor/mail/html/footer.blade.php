d1dd8376e3d3eb11752e4e0bba8fa7bc58e2a29b
Maria Sarazua <sarazuamaria@gmail.com>
2021-03-24 12:05:13 -0600

    Todo por el chino

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -14,17 +14,21 @@
                             }
 
                         </style>
-                        <a href=""><img style="width: 15%; float: left" class="user-avatar"
+                        <a href="http://ideacodex.co/"><img style="width: 15%; float: left" class="user-avatar"
                             src="https://bigbill.co/images/ideacode.png" alt="ideacode"></a>
+
                         <a href="https://www.facebook.com/ideacodex"><img style="width: 5%; float: right"
-                                class="user-avatar" src="{{ asset('images/facebook.png') }}" alt="facebook"></a>
+                                class="user-avatar" src="{{ asset('images/fcb.png') }}" alt="facebook"></a>
+
                         <a href="https://www.instagram.com/ideacodexgt/?hl=es"><img style="width: 5%; float: right"
-                                class="user-avatar" src="{{ asset('images/instagram.png') }}" alt="instagram"></a>
+                                class="user-avatar" src="{{ asset('images/ins.png') }}" alt="instagram"></a>
+
                         <a href="https://www.linkedin.com/company/ideacodex"><img style="width: 5%; float: right"
-                                class="user-avatar" src="{{ asset('images/linkedin.png') }}" alt="linkedin"></a>
+                                class="user-avatar" src="{{ asset('images/ln.png') }}" alt="linkedin"></a>
+                                
                         <a href="{{ url('https://api.whatsapp.com/send?phone=50233120413') }}"><img
                                 style="width: 5%; float: right" class="user-avatar"
-                                src="{{ asset('images/wps.png') }}" alt="whatsapp"></a>
+                                src="{{ asset('images/wtsp.png') }}" alt="whatsapp"></a>
                     </td>
                 </tr>
             </table>
