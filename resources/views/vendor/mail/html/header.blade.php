5cb877402a46c124ec4ae1f398399a72dc2afe08
Maria Sarazua <sarazuamaria@gmail.com>
2021-03-24 11:47:11 -0600

    Era el if xd :)

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
     <td class="header" align="center" style="background-color: black" width="570" cellpadding="0" cellspacing="0">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img style="width: 35%" class="user-avatar" src="https://bigbill.co/img/logo_tageta.png" alt="Big Bill">
-            @else
-                {{ $slot }}
-            @endif
+            <img style="width: 35%" class="user-avatar" src="https://bigbill.co/img/logo_tageta.png" alt="Big Bill">
         </a>
     </td>
 </tr>
